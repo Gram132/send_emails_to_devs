0212b7c4db793fdfbb1cbab4a56104b7a37d845f
Gram132 <84986668+Gram132@users.noreply.github.com>
2025-02-25 17:58:10 +0000

    Commit

--- get_sending_emails.py
+++ get_sending_emails.py
@@ -15,3 +15,18 @@ def get_developers_not_sent():
 
     return list(documents)
 
+def get_developers_not_sent_in(country):
+    """ Retrieve developers with 'is_sent' = False in random order """
+    
+    query = {"is_sent": False, "location":country}
+
+    # Use aggregation with $sample to shuffle the order randomly
+    documents = db.aggregate([
+        {"$match": query},
+        {"$sample": {"size": db.count_documents(query)}}  # Shuffle all matching documents
+    ])
+
+    return list(documents)
+
+get_developers_not_sent_in("Malaysia")
+
