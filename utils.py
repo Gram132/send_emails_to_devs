2ffc7e278bc62da5ce9fe14baae7af44ce565d9a
Gram132 <84986668+Gram132@users.noreply.github.com>
2025-02-27 17:51:46 +0000

    Commit

--- utils.py
+++ utils.py
@@ -3,4 +3,4 @@ import datetime
 def is_active_hour():
     """ Returns True if within active sending hours (9 AM - 6 PM) """
     now = datetime.datetime.now()
-    return 9 <= now.hour <= 18
+    return 1 <= now.hour <= 24
